c20cd5750023318c0c91a683dd87a8fc7fe9c0e8
Dimitri <dimak821@gmail.com>
2022-10-18 16:40:59 +0200

    First scratch
    update

--- game/Game.java
+++ game/Game.java
@@ -1,17 +1,34 @@
 package game;
 
-public class Game {
+public class Game implements Runnable {
 
     private Display display;
     public int width, height;
 
+    private Thread thread;
+
     public Game(String title, int width, int height){
         this.width = width;
         this.height = height;
-        
-        display = new Display(title, width, height);
 
+        display = new Display(title, width, height);
 
     }
 
+    public void run(){
+
+    }
+    public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop(){
+        try{
+            thread.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+    }
 }
