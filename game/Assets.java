7b941d399fd6c55bccf6c4317bcfc118deda3eb8
Dimitri <dimak821@gmail.com>
2022-10-26 03:03:38 +0200

    animations update
    added some fancy anim

--- game/Assets.java
+++ game/Assets.java
@@ -1,6 +1,8 @@
 package game;
 import java.awt.image.BufferedImage;
 
+import game.entities.creatures.Player;
+
 /*loading every images every sec is heavy
  * so let's make it simplier...
  * It will load once
@@ -8,7 +10,8 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
 
-    public static BufferedImage player,grass,fence,biom,water;
+    public static BufferedImage grass,fence,biom,water;
+    public static BufferedImage[] player_down,player_up,player_right,player_left,player_stop;
 
     public static final int width = 16, height = 16;
     
@@ -18,9 +21,27 @@ public class Assets {
         SpriteSheet PlayerSheet = new SpriteSheet(ImageLoader.loadImage("/game/res/textures/Sprout Lands/Characters/Basic Charakter Spritesheet.png"));
         SpriteSheet FenceSheet = new SpriteSheet(ImageLoader.loadImage("/game/res/textures/Sprout Lands/Tilesets/Fences.png"));
 
+        player_down = new BufferedImage[2];
+        player_up = new BufferedImage[2];
+        player_right = new BufferedImage[2];
+        player_left = new BufferedImage[2];
+        player_stop = new BufferedImage[2];
+
+        player_down[0] = PlayerSheet.crop(14*8,2*8,width,height);
+        player_down[1] = PlayerSheet.crop(20*8,2*8,width,height);
+        player_up[0] = PlayerSheet.crop(14*8,8*8,width,height);
+        player_up[1] = PlayerSheet.crop(20*8,8*8,width,height);
+        player_right[0] = PlayerSheet.crop(14*8,14*8,width,height);
+        player_right[1] = PlayerSheet.crop(20*8,14*8,width,height);
+        player_left[0] = PlayerSheet.crop(14*8,20*8,width,height);
+        player_left[1] = PlayerSheet.crop(20*8,20*8,width,height);
+
+        player_stop[0] = PlayerSheet.crop(16, 16, width, height);
+        player_stop[1] = PlayerSheet.crop(8*8, 16, width, height);
+        
         
         grass = GrassSheet.crop(0,80, width,height);
-        player = PlayerSheet.crop(16, 16, width, height);
+     
         fence = FenceSheet.crop(0, 0, width,height);
     }
     
--- game/entities/creatures/Player.java
+++ game/entities/creatures/Player.java
@@ -1,12 +1,20 @@
 package game.entities.creatures;
-
+import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 import java.awt.Color;
 import game.Assets;
+import game.animations.Animation;
 import game.handler.Handler;;
+
 
 public class Player extends Creatures {
 
+	//animations
+	private Animation animDown;
+	private Animation animUp;
+	private Animation animRight;
+	private Animation animLeft;
+	private Animation animStop;
 	
 	
 	public Player(Handler handler, float x, float y) {
@@ -17,10 +25,23 @@ public class Player extends Creatures {
 		bounds.y = 16;
 		bounds.width = 16;
 		bounds.height = 16;
+
+		animDown = new Animation(500, Assets.player_down);
+		animUp = new Animation(500, Assets.player_up);
+		animLeft = new Animation(500, Assets.player_left);
+		animRight = new Animation(500, Assets.player_right);
+		animStop = new Animation(500, Assets.player_stop);
+
 	}
 
 	@Override
 	public void tick() {
+		animDown.tick();
+		animUp.tick();
+		animRight.tick();
+		animLeft.tick();
+		animStop.tick();
+		//movements
 		getInput();
 		move();
 		handler.getGameCamera().centerOnEntity(this);
@@ -42,12 +63,26 @@ public class Player extends Creatures {
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()),  //centering our player as well
+		g.drawImage(getCurrentAnimationFrame(), (int) (x - handler.getGameCamera().getxOffset()),  //centering our player as well
 					(int) (y - handler.getGameCamera().getyOffset()), width, height, null);
 
-		g.setColor(Color.red);
-		g.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()), 
-		(int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
+		//g.setColor(Color.red);
+		//g.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()), 
+		//(int)(y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
 	}
 
+
+	private BufferedImage getCurrentAnimationFrame(){
+		if(xMove<0){
+			return animRight.getCurrentFrame();
+		}else if(xMove>0){
+			return animLeft.getCurrentFrame();
+		}else if(yMove<0){
+			return animUp.getCurrentFrame();
+		}else if(yMove>0){
+			return animDown.getCurrentFrame();
+		}else{
+			return animStop.getCurrentFrame();
+		}
+	}
 }
